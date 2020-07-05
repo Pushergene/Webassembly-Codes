02b8e94f179db957bde716f0f2035188eb772c2f
Pushergene <pushergene@protonmail.com>
2020-07-05 19:10:27 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,42 @@
 #define WASM_EXPORT __attribute__((visibility("default")))
 
 WASM_EXPORT
+#include <iostream>
+
+using namespace std;
+
+void teppich()
+{
+  int ebayteppich = 923823;
+  printf("Teppich: %d \n", ebayteppich);
+  if (ebayteppich < -1) {
+      printf("Alter, was geht da ab?!");
+  }
+  else {
+      printf("Okay, geht klar!");
+  }
+}
+
+void firefox() {
+    string Frage = "Was für eine Version hat dein Firefox?";
+    string Firefox;
+    scanf(Firefox)
+    if(Frage < 78) {
+        printf("Dein Firefox ist sicher.", Frage);
+    }
+    else if (Firefox > 78) {
+        printf("Du hast eine instabile Firefox version.");
+    }
+    else if (Firefox > 68 && Firefox < 69) {
+        printf("Dein Firefox hat LTS, LongTimeSupport");
+}
+    else {
+        printf("Bitte überleg dir, ob du unsicher surfen willst.");
+    }
+}
+
 int main() {
-  long ebayteppich = 92387283923.1239813;
-  printf("Teppich: %d \n", sizeof(ebayteppich));
-  return 42;
+    teppich();
+    
+}
 }
