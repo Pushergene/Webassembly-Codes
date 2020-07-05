db63199bfa3b4ac6023ec5691038f917a333c2ee
Pushergene <pushergene@protonmail.com>
2020-07-05 20:16:27 +0200

    Update build.ts

--- build.ts
+++ build.ts
@@ -2,8 +2,8 @@ import * as gulp from "gulp";
 import { Service, project } from "@wasm/studio-utils";
 
 gulp.task("build", async () => {
-  const data = await Service.compileFile(project.getFile("src/main.c"), "c", "wasm", "-g -O3");
-  const outWasm = project.newFile("out/main.wasm", "wasm", true);
+  const data = await Service.compileFile(project.getFile("main.c"), "c", "wasm", "-g -O3");
+  const outWasm = project.newFile("main.wasm", "wasm", true);
   outWasm.setData(data);
 });
 
